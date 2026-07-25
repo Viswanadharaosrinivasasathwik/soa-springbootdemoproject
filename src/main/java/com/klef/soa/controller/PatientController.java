04c8d8f493e5f33eaa41e8736d132202b70f46eb
sathwik <2400030159@kluniversity.in>
2026-07-25 09:12:54 +0530

    Project v2.0

--- src/main/java/com/klef/soa/controller/PatientController.java
+++ src/main/java/com/klef/soa/controller/PatientController.java
@@ -1,7 +1,10 @@
 package com.klef.soa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.klef.soa.service.PatientService;
@@ -20,4 +23,21 @@ public class PatientController
 	  return "SOA Programming & Microservices";
 	  
   }
+	
+	@GetMapping("")
+	public void displayallPatients()
+	{
+		List<Patient> patients = service.displayAllPatients();
+		return ResponseEntity.status(200).body(patients);
+	}
+	
+	@PostMapping("/add")
+	public ResponseEntity<Patient> addpatient(@RequestBody Patient p)
+	{
+	Patient patient = service.addPatient(p);
+	return ResponseEntity.status(201).body(patient);
+	}
+	
+	
+	
 }
